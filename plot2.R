f24146c9e8675d77da3ee313796b80ce914a9d21
i81017 <ta_kabina@yahoo.com>
2016-05-10 00:53:49 +0545

    edits

--- plot2.R
+++ plot2.R
@@ -1,3 +1,5 @@
+## plot2
+
 if (!file.exists("data.zip")) {
   download.file(url="https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
                 destfile="data.zip",
